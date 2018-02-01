2b914e4efb3b723416570f984bb125c0b29c7cd3
Cali93 <c.armut93@outlook.com>
2018-02-01 21:40:31 +0100

    basic template is on point

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './components/register/register.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { Featuresv1Component } from './components/featuresv1/featuresv1.component';
+import { Featuresv3Component } from './components/featuresv3/featuresv3.component';
+import { CardComponent } from './components/card/card.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { RegisterformComponent } from './components/registerform/registerform.component';
+import { LoginformComponent } from './components/loginform/loginform.component';
+import { JumbotronComponent } from './components/jumbotron/jumbotron.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    NavbarComponent,
+    CarouselComponent,
+    Featuresv1Component,
+    Featuresv3Component,
+    CardComponent,
+    FooterComponent,
+    RegisterformComponent,
+    LoginformComponent,
+    JumbotronComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
+  schemas: [ NO_ERRORS_SCHEMA ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
